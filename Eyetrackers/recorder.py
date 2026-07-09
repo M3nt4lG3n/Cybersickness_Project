bd1e5c4dc9a2e5fdb52d8d092b7b329d51dbb8a1
M3nt4lG3n <b-bizon@onu.edu>
2026-07-09 13:42:04 -0400

    Implement StereoRecorder class for video recording

--- Eyetrackers/recorder.py
+++ Eyetrackers/recorder.py
@@ -1,1 +1,68 @@
+import cv2
+import os
+import time
 
+
+class StereoRecorder:
+
+
+    def __init__(
+        self,
+        left_file,
+        right_file,
+        fps,
+        width,
+        height
+    ):
+
+
+        fourcc = cv2.VideoWriter_fourcc(
+            *"mp4v"
+        )
+
+
+        self.left = cv2.VideoWriter(
+            left_file,
+            fourcc,
+            fps,
+            (width,height)
+        )
+
+
+        self.right = cv2.VideoWriter(
+            right_file,
+            fourcc,
+            fps,
+            (width,height)
+        )
+
+
+        self.frames = 0
+
+
+
+    def write(
+        self,
+        pair
+    ):
+
+
+        self.left.write(
+            pair.left.image
+        )
+
+
+        self.right.write(
+            pair.right.image
+        )
+
+
+        self.frames += 1
+
+
+
+    def close(self):
+
+        self.left.release()
+
+        self.right.release()
